1af513acc1f5015a5bb368c7e78753594274cf43
Changbum97 <chb2005@naver.com>
2022-11-14 16:32:42 +0900

    Programmers p12947 하샤드 수

---
+++ src/Programmers/digit/p12947.java
@@ -0,0 +1,28 @@
+package Programmers.digit;
+
+public class p12947 {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(10));
+        System.out.println(s.solution(11));
+        System.out.println(s.solution(12));
+        System.out.println(s.solution(13));
+    }
+
+    static class Solution{
+        public boolean solution(int x) {
+            int sumOfDigit = 0;
+
+            int temp = x;
+            while(temp != 0) {
+                sumOfDigit += temp % 10;
+                temp /= 10;
+            }
+
+            if(x % sumOfDigit == 0) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
