36b7a3a690e9c65349cefd68bc7a3625048c44b3
Changbum97 <chb2005@naver.com>
2023-05-23 19:59:46 +0900

    BOJ p9655 돌 게임

---
+++ src/BOJ/dp/p9655.java
@@ -0,0 +1,12 @@
+package BOJ.dp;
+
+import java.util.Scanner;
+
+public class p9655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String str = n % 2 == 0 ? "CY" : "SK";
+        System.out.println(str);
+    }
+}
