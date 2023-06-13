227329ad07590d76a4acc4537040ae2729b093c3
Changbum97 <chb2005@naver.com>
2023-06-13 18:35:38 +0900

    테스트 케이스 생성 코드

--- src/_MakeTestCase/Make.java
+++ src/_MakeTestCase/Make.java
@@ -1,2 +1,20 @@
-package _MakeTestCase;public class Make {
+package _MakeTestCase;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Make {
+    public static void main(String[] args) throws IOException {
+        String path = System.getProperty("user.dir") + "/src/_MakeTestCase/";
+        PrintWriter pw = new PrintWriter(new FileWriter(path + "output.txt"));
+
+        int n = 1000000;
+        pw.println(n);
+        for (int i = 0 ; i < n ; i ++) {
+            pw.println(i);
+        }
+
+        pw.close();
+    }
 }
