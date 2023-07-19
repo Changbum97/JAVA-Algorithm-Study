b801573843783dc652c19e84da5fbe1a1aa09572
Changbum97 <chb2005@naver.com>
2023-07-19 18:08:08 +0900

    코딩테스트 대비 - 이분탐색

---
+++ src/Programmers/_codingTest/BinarySearch_Example.java
@@ -0,0 +1,51 @@
+package Programmers._codingTest;
+
+public class BinarySearch_Example {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new int[]{2, 4, 5, 3, 2, 1, 4, 2, 5, 1}, 3));
+        System.out.println(s.solution(new int[]{5}, 1));
+    }
+
+    static class Solution {
+        public int solution(int[] stones, int k) {
+            int answer = 0;
+
+            int left = stones[0];
+            int right = stones[0];
+            for (int i = 0; i < stones.length; i++) {
+                left = Math.min(left, stones[i]);
+                right = Math.max(right, stones[i]);
+            }
+
+            while (left <= right) {
+                int mid = (left + right) / 2;
+
+                boolean pass = true;
+                int cnt = 0;
+                for (int i = 0; i < stones.length; i++) {
+                    if (stones[i] < mid) {
+                        cnt++;
+                    } else {
+                        cnt = 0;
+                    }
+
+                    if (cnt == k) {
+                        pass = false;
+                        break;
+                    }
+                }
+
+                if (pass == false) {
+                    right = mid - 1;
+                } else {
+                    answer = mid;
+                    left = mid + 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
