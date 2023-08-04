233514762d3f3e8238e773414a8787fc130f3c2a
Changbum97 <chb2005@naver.com>
2023-08-04 14:52:16 +0900

    코딩테스트 대비 - Hash, Set, Arrays.Sort를 사용한 예제

---
+++ src/Programmers/_coding_test/Hash_Set_Sort_Example.java
@@ -0,0 +1,45 @@
+package Programmers._coding_test;
+
+/**
+ * 전화번호 목록
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42577
+ * Hash와 Set을 사용하고 Arrays.Sort 설정하여 사용한 예제
+ */
+
+import java.util.*;
+
+public class Hash_Set_Sort_Example {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new String[]{"119", "97674223", "1195524421"}) == false);
+        System.out.println(s.solution(new String[]{"123", "456", "789"}) == true);
+        System.out.println(s.solution(new String[]{"12", "123", "1235", "567", "88"}) == false);
+        System.out.println(s.solution(new String[]{"12", "2121212", "1"}) == false);
+    }
+
+    static class Solution {
+        public boolean solution(String[] phone_book) {
+
+            // 문자열 길이를 기준으로 오름차순 정렬
+            Arrays.sort(phone_book, (o1, o2) -> {
+                return o1.length() - o2.length();
+            });
+
+            Set<String> set = new HashSet<>();
+            for(String phone : phone_book) {
+
+                String temp = "";
+                for (int i = 0 ; i < phone.length() ; i ++) {
+                    temp += phone.charAt(i);
+                    if (set.contains(temp)) {
+                        return false;
+                    }
+                }
+                set.add(phone);
+            }
+
+            return true;
+        }
+    }
+}
