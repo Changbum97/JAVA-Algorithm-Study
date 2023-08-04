bb3f0c087ca24cd8b91fa7d174aef38a464889a2
Changbum97 <chb2005@naver.com>
2023-08-04 14:36:00 +0900

    코딩테스트 대비 - Hash, Map을 이용한 예제

--- src/Programmers/_coding_test/Hash_Map_Example.java
+++ src/Programmers/_coding_test/Hash_Map_Example.java
@@ -1,2 +1,44 @@
-package Programmers._coding_test;public class Hash_Map_Example {
+package Programmers._coding_test;
+
+/**
+ * 완주하지 못한 선수
+ * https://school.programmers.co.kr/learn/courses/30/lessons/42576
+ * Hash와 Map을 이용한 예제
+ */
+
+import java.util.*;
+
+public class Hash_Map_Example {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(new String[]{"leo", "kiki", "eden"}, new String[]{"eden", "kiki"}).equals("leo"));
+        System.out.println(s.solution(new String[]{"marina", "josipa", "nikola", "vinko", "filipa"}, new String[]{"josipa", "filipa", "marina", "nikola"}).equals("vinko"));
+        System.out.println(s.solution(new String[]{"mislav", "stanko", "mislav", "ana"}, new String[]{"stanko", "ana", "mislav"}).equals("mislav"));
+    }
+
+    static class Solution {
+        public String solution(String[] participant, String[] completion) {
+
+            Map<String, Integer> map = new HashMap<>();
+            for (String p : participant) {
+                if (map.containsKey(p)) {
+                    map.put(p, map.get(p) + 1);
+                } else {
+                    map.put(p, 1);
+                }
+            }
+
+            for (String c : completion) {
+                int cnt = map.get(c);
+                if (cnt == 1) {
+                    map.remove(c);
+                } else {
+                    map.put(c, cnt - 1);
+                }
+            }
+
+            return (String) map.keySet().toArray()[0];
+        }
+    }
 }
