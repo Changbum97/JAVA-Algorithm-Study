a0c74b7feda7177e548d3f7a6cbef8a82b5cd7e6
Changbum97 <chb2005@naver.com>
2023-08-20 18:13:14 +0900

    BOJ p1929 소수구하기

---
+++ src/BOJ/mathematics/primeNumber/p1929.java
@@ -0,0 +1,41 @@
+package BOJ.mathematics.primeNumber;
+
+/**
+ * 소수 구하기
+ * https://www.acmicpc.net/problem/1929
+ */
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p1929 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        boolean[] isPrimeNumber = getPrimeNumbers(n);
+        for (int i = m ; i <= n ; i ++) {
+            if (isPrimeNumber[i]) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    private static boolean[] getPrimeNumbers(int n) {
+        boolean[] isPrimeNumber = new boolean[n + 1];
+        Arrays.fill(isPrimeNumber, true);
+        isPrimeNumber[1] = false;
+
+        for (int i = 2 ; i * i <= n ; i ++) {
+            if (isPrimeNumber[i]) {
+                for (int j = i + i ; j <= n ; j += i) {
+                    isPrimeNumber[j] = false    ;
+                }
+            }
+        }
+
+        return isPrimeNumber;
+    }
+}
