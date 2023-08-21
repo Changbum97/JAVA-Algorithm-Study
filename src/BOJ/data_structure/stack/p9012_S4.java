8d540abdf8304befc89c2a2922b2fe7d2387e39f
Changbum97 <chb2005@naver.com>
2023-08-21 16:51:37 +0900

    BOJ p9012 괄호

---
+++ src/BOJ/data_structure/stack/p9012_S4.java
@@ -0,0 +1,43 @@
+package BOJ.data_structure.stack;
+
+/**
+ * 괄호
+ * 실버 4
+ * https://www.acmicpc.net/problem/9012
+ */
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class p9012_S4 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0 ; i < n ; i ++) {
+            String str = sc.nextLine();
+            System.out.println(check(str));
+        }
+    }
+
+    private static String check(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0 ; i < str.length() ; i ++) {
+            if (str.charAt(i) == '(') {
+                stack.add('(');
+            } else {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return "NO";
+                }
+            }
+        }
+
+        if (!stack.isEmpty()) {
+            return "NO";
+        }
+        return "YES";
+    }
+}
