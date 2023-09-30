19b3e7e8901454b6af9e244b10a16cc68c040812
Changbum97 <chb2005@naver.com>
2023-09-30 15:51:10 +0900

    BOJ p10926 ??!

---
+++ src/BOJ/etc/p10926.java
@@ -0,0 +1,11 @@
+package BOJ.etc;
+
+import java.util.Scanner;
+
+public class p10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str + "??!");
+    }
+}
