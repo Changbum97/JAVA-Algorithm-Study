991b88e2ee8b7e48f8c2b65c27941d4a009764fb
Changbum97 <chb2005@naver.com>
2023-10-23 20:25:36 +0900

    BOJ p10952 A+B-5

---
+++ src/BOJ/etc/p10952.java
@@ -0,0 +1,18 @@
+package BOJ.etc;
+
+import java.util.Scanner;
+
+public class p10952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x == 0 && y == 0) {
+                break;
+            }
+            System.out.println(x + y);
+        }
+    }
+}
